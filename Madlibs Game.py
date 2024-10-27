cfe1975b5423e638789f4a598f18113312971415
Nuarat Jahan Shawon <nusrat.shawona27@gmail.com>
2024-10-28 01:32:31 +0600

    1.31 AM

---
+++ Madlibs Game.py
@@ -0,0 +1,13 @@
+#madlibs game
+#word game where u create a story by filling in blacks with random words
+
+adjective1 =input("Enter an adjective (description):")
+noun1= input("enter a noun (person,place,thing):")
+adjective2= input("entr and adjective (description):")
+verb1= input("Enter a verb ending with 'ing'")
+adjective3=input("enter an adjective (description):")
+
+print(f"today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1} .")
+print(f"{noun1} was {adjective2} and {verb1} ")
+print(f"I was {adjective3} !")
